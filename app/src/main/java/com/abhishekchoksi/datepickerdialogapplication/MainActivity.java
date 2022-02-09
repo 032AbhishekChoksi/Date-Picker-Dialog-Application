a9e80ec92f1a9a19d490ec5b0c74e80a5b1a94e8
032 - Abhishek Choksi <19bmiit032@gmail.com>
2022-02-09 22:43:33 +0530

    Date Picker Dialog Application @2022 #032AbhishekChoksi. All rights reserved.

---
+++ app/src/main/java/com/abhishekchoksi/datepickerdialogapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.abhishekchoksi.datepickerdialogapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvDate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvDate = findViewById(R.id.tv_date);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                                month = month + 1;
+                                String date = day + "/" + month + "/" + year;
+                                tvDate.setText(date);
+                            }
+                        }, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+    }
+}
